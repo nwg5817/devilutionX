e5cfa87f363d48dc4c586bd6a9fb6e2932af4312
nwg5817 <nwg5817@gmail.com>
2020-08-04 12:21:18 -0400

    Fast walk in town

--- Source/player.cpp
+++ Source/player.cpp
@@ -1150,11 +1150,26 @@ void PM_ChangeOffset(int pnum)
 	}
 
 	plr[pnum]._pVar8++;
+	if (currlevel == 0) {
+		plr[pnum]._pVar8++;
+	}
 	px = plr[pnum]._pVar6 / 256;
 	py = plr[pnum]._pVar7 / 256;
 
-	plr[pnum]._pVar6 += plr[pnum]._pxvel;
-	plr[pnum]._pVar7 += plr[pnum]._pyvel;
+	//	plr[pnum]._pVar6 += plr[pnum]._pxvel;
+	//	plr[pnum]._pVar7 += plr[pnum]._pyvel;
+	// speedup run animation: //TODO bad place
+	if (currlevel == 0) {
+		if (plr[pnum]._pAnimFrame % 2 == 1) {
+			plr[pnum]._pAnimFrame++;
+		}
+		if (plr[pnum]._pAnimFrame >= plr[pnum]._pWFrames) {
+			plr[pnum]._pAnimFrame = 0;
+		}
+	}
+
+	plr[pnum]._pVar6 += (currlevel == 0 ? 2 : 1) * plr[pnum]._pxvel;
+	plr[pnum]._pVar7 += (currlevel == 0 ? 2 : 1) * plr[pnum]._pyvel;
 	plr[pnum]._pxoff = plr[pnum]._pVar6 / 256;
 	plr[pnum]._pyoff = plr[pnum]._pVar7 / 256;
 
